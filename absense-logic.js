92ae19e889d7ca79760a2910ed60c68bf681e0a1
alexhernandezord <44363090+alexhernandezord@users.noreply.github.com>
2019-07-13 13:13:15 +0200

    more random periods, min options changed +15 to +10 & timezone fixed for summer time (#1)
    * more random periods, min options changed +15 to +10 & timezone fixed for summer time
    
    * Fix length get out (NaN error) & re-format

--- absense-logic.js
+++ absense-logic.js
@@ -2,30 +2,35 @@ const base_url = 'https://app.absence.io/api/v2/'
 console.log('date', date)
 
 getUserId(user_token).then((user_info) => {
-  var url = base_url + 'timespans/create?cachekiller='+ Date.now() +'&v=0.10.329'
+  var url = base_url + 'timespans/create?cachekiller=' + Date.now() + '&v=0.10.329'
   var user_id = user_info._id
 
-  var mins_options = [0,15,30,45,60]
+  var start_mins_options = [10, 20, 30, 40, 50]
+  var break_mins_options = [-10, 0, 10]
+  var variable_mins_options = [-30, -20, -10, 10, 20, 30]
 
+  var start_mins = start_mins_options[Math.round(Math.random() * (start_mins_options.length - 1))]
+  var break_mins = break_mins_options[Math.round(Math.random() * (break_mins_options.length - 1))]
+  var variable_mins = variable_mins_options[Math.round(Math.random() * (variable_mins_options.length - 1))]
 
   // crear una funcion para generar intervalos de fechas
   var init_date_start = new Date(date)
   var init_date_end = new Date(date)
-  
+
   var break_date_start = new Date(date)
   var break_date_end = new Date(date)
 
   var end_date_start = new Date(date)
   var end_date_end = new Date(date)
 
-  init_date_start.setHours(11, mins_options[Math.round(Math.random()*mins_options.length)])
-  init_date_end.setHours(init_date_start.getHours()+4, init_date_start.getMinutes())
-  
-  break_date_start.setHours(init_date_end.getHours(),init_date_end.getMinutes())
-  break_date_end.setHours(break_date_start.getHours()+1, break_date_start.getMinutes())
-  
+  init_date_start.setHours(9, start_mins)
+  init_date_end.setHours(init_date_start.getHours() + 4, init_date_start.getMinutes() + variable_mins)
+
+  break_date_start.setHours(init_date_end.getHours(), init_date_end.getMinutes())
+  break_date_end.setHours(break_date_start.getHours() + 1, break_date_start.getMinutes() + break_mins)
+
   end_date_start.setHours(break_date_end.getHours(), break_date_end.getMinutes())
-  end_date_end.setHours(end_date_start.getHours()+4, end_date_start.getMinutes())
+  end_date_end.setHours(end_date_start.getHours() + 4, end_date_start.getMinutes() - variable_mins)
 
   // /func
 
@@ -48,36 +53,36 @@ getUserId(user_token).then((user_info) => {
   ]
 
   var requests = []
-  
-  config.forEach(( entry, index ) => {
-    var payload = {'userId': user_id,'commentary':'','_id':'new','timezone':'+0000','timezoneName':'Central European Summer Time','source':{'sourceType':'browser','sourceId':'manual'},'type':'work','start':'2019-05-21T07:30:00Z','end':'2019-05-21T12:00:00Z','trace':[]}
+
+  config.forEach((entry, index) => {
+    var payload = { 'userId': user_id, 'commentary': '', '_id': 'new', 'timezone': '+0200', 'timezoneName': 'Central European Summer Time', 'source': { 'sourceType': 'browser', 'sourceId': 'manual' }, 'type': 'work', 'start': '2019-05-21T07:30:00Z', 'end': '2019-05-21T12:00:00Z', 'trace': [] }
     var d = new Date(date)
-  
+
     //d.setDate(d.getDate());
-  
+
     payload.start = entry.start
     payload.end = entry.end
     payload.type = entry.type
-  
+
     var request = new Request(url, {
-        method: 'POST', 
-        body: JSON.stringify(payload),
-        headers: new Headers({
-            'Content-Type':'application/json',
-            'x-languagetoken': 'es',
-            'X-Requested-With': 'XMLHttpRequest',
-            'x-vacationtoken': user_token
-        })
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'x-languagetoken': 'es',
+        'X-Requested-With': 'XMLHttpRequest',
+        'x-vacationtoken': user_token
+      })
     })
-    
+
     console.log('request', request)
     requests.push(request)
-  
+
     console.log('entry', entry)
     console.log('payload', payload)
   })
 
-  Promise.all(requests.map( request => fetch(request) )).then(responses =>
+  Promise.all(requests.map(request => fetch(request))).then(responses =>
     Promise.all(responses.map(res => res.text()))
   ).then(texts => {
     console.log('texts', texts)
@@ -94,8 +99,8 @@ function getUserId(_user_token) {
   var user_info_request = new Request(auth_url, {
     method: 'GET'
   })
-  console.log('?',auth_url)
+  console.log('?', auth_url)
   return fetch(user_info_request)
-  .then(response => response.json())
-  .then(data => data)
+    .then(response => response.json())
+    .then(data => data)
 }
--- popup.js
+++ popup.js
@@ -10,24 +10,24 @@ function click(e) {
   chrome.tabs.create({
     active: false,
     url: 'https://app.absence.io/#/timetracking'
-}, function(tab) {
+  }, function (tab) {
     chrome.tabs.executeScript(tab.id, {
-        code: 'var date = '+ value + '; var user_token = JSON.parse(window.localStorage.getItem("absenceConnectedAccounts"))[window.localStorage.getItem("activeid")].ajaxToken;'
-    }, function() {
+      code: 'var date = ' + value + '; var user_token = JSON.parse(window.localStorage.getItem("absenceConnectedAccounts"))[window.localStorage.getItem("activeid")].ajaxToken;'
+    }, function () {
       chrome.tabs.executeScript(tab.id,
-        {file:'/absense-logic.js'}, function(){
+        { file: '/absense-logic.js' }, function () {
           input_date.disabled = false
           btn.disabled = false
         });
     });
-}); 
+  });
 
 
- /*  
-  chrome.tabs.executeScript(null, {code: ''}, function(){
-    chrome.tabs.executeScript(null,
-      {file:'/absense-logic.js'});
-  }); */
+  /*  
+   chrome.tabs.executeScript(null, {code: ''}, function(){
+     chrome.tabs.executeScript(null,
+       {file:'/absense-logic.js'});
+   }); */
   //window.close();
 }
 
